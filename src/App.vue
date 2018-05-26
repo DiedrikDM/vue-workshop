0a9f9f07724b441aa9bbb67dd1ed1f731ad29016
Diedrik De Mits <diedrikdm@hotmail.com>
2018-05-26 15:40:38 +0200

    Initializing Awesome Cocktails

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data: function() { }
+}
+</script>
+
+<style>
+</style>
